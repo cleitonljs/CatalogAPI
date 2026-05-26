2ba25572441d97b10775bc16cd47071aba3dae98
cleitonljs <cleitonljs@gmail.com>
2026-05-26 11:57:28 -0300

    Adicionar arquivos de projeto.

---
+++ CatalogAPI/Controllers/HealthController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CatalogAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Health: OK";
+        }
+
+
+    }
+}
